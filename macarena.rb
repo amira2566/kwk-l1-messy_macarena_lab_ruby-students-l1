3e58d86a8ec3fe7c9613cf7a3e763f390d7f939e
amira2566 <braemira2@icloud.com>
2018-07-09 01:43:56 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -2,6 +2,12 @@ def macarena_intro
   puts "How to do the macarena"
 end 
 macarena_intro 
+
+
+
+
+
+
 def macarena
   puts "Right arm out in front of you, palm facing down.", "Left arm out in front of you, palm facing down.", "Turn your right palm up.", "Turn your left palm up.", "Turn your left palm up.", "Place your right hand on your left shoulder.", "Place your left hand on your right shoulder.", "Bring your right hand up and around to the back of your head.", "Bring your left hand up and around to the back your head."
 end
