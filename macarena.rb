71e8f040171c66b1273cc3d675a1584bd0d9d253
amira2566 <braemira2@icloud.com>
2018-07-09 01:39:05 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,4 +1,4 @@
-def macarena.rb
+def macarena
   puts "Right arm out in front of you, palm facing down.", "Left arm out in front of you, palm facing down.", "Turn your right palm up.", "Turn your left palm up.", "Turn your left palm up.", "Place your right hand on your left shoulder.", "Place your left hand on your right shoulder.", "Bring your right hand up and around to the back of your head.", "Bring your left hand up and around to the back your head."
 end
-puts macarena.rb
+macarena.rb
