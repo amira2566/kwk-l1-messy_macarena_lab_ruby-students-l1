4756261f572815c7a87429d7fc606643cc3bae97
amira2566 <braemira2@icloud.com>
2018-07-09 01:31:22 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,2 +1,3 @@
 def macarena.rb
-  puts "Right arm out in front of you, palm facing down.", "Left arm out in front of you, palm facing down.", "Turn your right palm up.", "Turn your left palm up.", "Turn your left palm up.", "Place your right hand on your left shoulder.", "Place your left hand on your right shoulder.", "Bring your right hand up and around to the back of your head.", ""
+  puts "Right arm out in front of you, palm facing down.", "Left arm out in front of you, palm facing down.", "Turn your right palm up.", "Turn your left palm up.", "Turn your left palm up.", "Place your right hand on your left shoulder.", "Place your left hand on your right shoulder.", "Bring your right hand up and around to the back of your head.", "Bring your left hand up and around to the back your head."
+end
